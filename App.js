45309cb3c8cf8b427b75d58ef7a7e88348a14a2a
Rista99 <aleksaristicnk@gmail.com>
2023-08-18 12:58:57 +0200

    Firestore setup

--- App.js
+++ App.js
@@ -1,17 +1,9 @@
 import React from 'react';
-import {View, Text, StyleSheet, StatusBar} from 'react-native';
-import Header from './src/components/Header';
-import {colors} from './src/global/styles';
+import {StyleSheet} from 'react-native';
 import RootNavigator from './src/navigation/RootNavigator';
-import HomeScreen from './src/screens/HomeScreen';
 
 export default function App() {
   return <RootNavigator />;
-  return (
-    <View style={styles.container}>
-      <Header title="OVO JE TITLE" iconLeft="arrow-left" />
-    </View>
-  );
 }
 
 const styles = StyleSheet.create({
