1daf9c8a4e04f3919332568609fcd0d069b8696d
n-vojin <80399345+n-vojin@users.noreply.github.com>
2023-08-18 22:05:29 +0200

    MASA ERRORA!

--- src/screens/RestaurantsMapScreen.js
+++ src/screens/RestaurantsMapScreen.js
@@ -2,11 +2,11 @@ import React from 'react';
 import {View, Text} from 'react-native';
 import HeaderHomeScreen from '../components/HeaderHomeScreen';
 
-export default function MyAccountScreen() {
+export default function RestaurantsMapScreen() {
   return (
     <View>
       <HeaderHomeScreen iconLeft="menu" />
-      <Text>MyAccountScreen</Text>
+      <Text>RestaurantsMapScreen</Text>
     </View>
   );
 }
